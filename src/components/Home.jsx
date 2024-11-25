4ccde0c0d7cbbac1f95ad2b3a3f2a8f6fd319dba
sougata <sougataghar47@gmail.com>
2024-11-26 03:24:14 +0530

    added projects to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 function Home(){
     return <div>
         <h1 className="text-4xl  text-white">Welcome To My Website</h1>
@@ -5,6 +6,10 @@ function Home(){
         <img loading="lazy" className="rounded-2xl" src="/pexels-photo-3826666.jpeg" />
         </div>
         <p className="text-white leading-7 italic font-bold text-xl"> My name is Sougata Ghar and I am a front end web developer who is proficient in HTML, CSS, Javascript,React,Vue,Next JS,Express JS and MongoDB. </p>
+        <Link to={'/projects'}>
+        <button className="btn mt-4 w-full bg-white text-black hover:bg-slate-200">Check Out My Projects</button>
+        </Link>
+                
     </div>
 }
 export default Home;
