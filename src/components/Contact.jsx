1efd8862a372092b55d1b834ed59ca21c2190d35
sougata <sougataghar47@gmail.com>
2024-11-26 03:15:10 +0530

    lazy loading

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ function Contact(){
     return <div>
         <h1 className="text-4xl  text-white">Contact</h1>
         <div className=" my-8 w-full md:w-[440px]">
-        <img className=" rounded-2xl" src="/phone.jpeg" />
+        <img loading="lazy" className=" rounded-2xl" src="/phone.jpeg" />
         </div>
         <div className="my-8 text-white">
             <h1 className="text-4xl ">Email 📬</h1>
