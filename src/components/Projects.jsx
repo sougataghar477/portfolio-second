db245dcdccd6c555debe15fb7f38fb93e417d36d
sougata <sougataghar47@gmail.com>
2024-11-25 18:04:59 +0530

    added plantly

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,9 @@ import Card from "./Card";
 function Projects(){
     let projects = [
         { heading:'TimeFramed',text:'Made some parts of the website with React JS',link:'https://timeframed.art'},
-        { heading: 'PersonalFinance', text: 'A website I made with react', link: 'https://www.personalfinances.stage.iamthedev.eu/' },
+        {heading:'Plantly',text:'A fullstack E-Commerce website made with Next JS,Chakra UI,Stripe',link:'https://plantly-rho.vercel.app/',github:'https://github.com/sougataghar477/plantly'},
         { heading: 'Rabbit API', text: 'A fullstack open source Rabbit Image API made with Next JS,Chakra UI,MongoDB and Firebase.', link: 'https://rabbit-api-two.vercel.app/', github: 'https://github.com/sougataghar477/rabbit-api' },
+        { heading: 'PersonalFinance', text: 'A website I made with react', link: 'https://www.personalfinances.stage.iamthedev.eu/' },
         { heading: 'Kontrast Productions', text: 'Made the portfolio and contact sections', link: 'https://www.kontrastproduction.sk/#testimonials' },
         { heading:'Todos App',text:'A full stack todos app made with Next JS,Mantine',link:'https://test-todos-next.vercel.app/',github:'https://github.com/sougataghar477/test-todos-next'},
         {heading:'Alveus Sanctuary',text:'Contributed to website of Alveus Sanctuary',link:'https://www.alveussanctuary.org/',github:'https://github.com/alveusgg/alveusgg/tree/main/apps/website'},
