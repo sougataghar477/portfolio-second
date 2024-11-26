4174339bdfd8bdc1277dc7dd59a33888ff4a3319
sougata <sougataghar47@gmail.com>
2024-11-26 08:18:42 +0530

    adjusted font weight of button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home(){
         </div>
         <p className="text-white leading-7 italic font-bold text-xl"> My name is Sougata Ghar and I am a front end web developer who is proficient in HTML, CSS, Javascript,React,Vue,Next JS,Express JS and MongoDB. </p>
         <Link to={'/projects'}>
-        <button className="btn mt-4 w-full bg-white text-black hover:bg-slate-200">Check Out My Projects</button>
+        <button className="btn font-bold mt-4 w-full bg-white text-black hover:bg-slate-200">Check Out My Projects</button>
         </Link>
                 
     </div>
