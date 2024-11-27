d2d905f2ef3dfbf64bf4ac71407a8a51f7594986
sougata <sougataghar47@gmail.com>
2024-11-27 17:23:58 +0530

    changed styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,12 +7,12 @@ function Card({ project }) {
       <p className="leading-6 mb-4 italic text-white">{project.text}</p>
       <div className="flex gap-4">
         <a target="_blank" href={project.link}>
-          <button className="btn bg-[#2cb0ff] font-bold border-0  text-[#fff] w-28 hover:bg-sky-500">
+          <button className="btn bg-[#2cb0ff] font-bold border-0  text-[#fff] w-28 hover:bg-sky-400">
             Check Out
           </button>
         </a>
         {project.github && <a target="_blank" href={project.github}>
-          <button className="btn  bg-[#2cb0ff] border-0 font-bold  text-[#fff] w-28 hover:bg-sky-300">
+          <button className="btn  bg-[#2cb0ff] border-0 font-bold  text-[#fff] w-28 hover:bg-sky-400">
             Github
           </button>
         </a>}
