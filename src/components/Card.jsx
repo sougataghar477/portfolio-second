ac755658b25f9c2d439b1f25ebdf6fedcd975846
sougata <sougataghar47@gmail.com>
2024-11-28 09:04:55 +0530

    changed button text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
 
 function Card({ project }) {
-  return <div className="card scale-card relative bg-base-300   shadow-xl mb-8 w-full">
-     
+  return <div className="card scale-card relative bg-base-300   mb-8 w-full">
+
     <div className="card-body">
       <h1 className="card-title text-white">{project.heading}</h1>
       <p className="leading-6 mb-4 italic text-white">{project.text}</p>
       <div className="flex gap-4">
         <a target="_blank" href={project.link}>
-          <button className="btn bg-[#2cb0ff] font-bold transition-all border-0  text-white w-28 hover:bg-sky-300">
+          <button className="btn bg-gradient-to-r from-cyan-500 to-blue-500 font-bold transition-all border-0  text-white w-28 hover:from-cyan-400 hover:to-blue-400">
             Visit Site
           </button>
         </a>
         {project.github && <a target="_blank" href={project.github}>
-          <button className="btn  bg-[#2cb0ff] border-0 font-bold transition-all text-white w-28 hover:bg-sky-300">
+          <button className="btn  bg-gradient-to-r from-cyan-500 to-blue-500 border-0 font-bold transition-all text-white w-28 hover:from-cyan-400 hover:to-blue-400">
             Github
           </button>
         </a>}
