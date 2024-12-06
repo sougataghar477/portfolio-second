5a989074b688c6098f9ed10afdcd141178711ff7
sougata <sougataghar47@gmail.com>
2024-12-06 18:20:33 +0530

    changed card color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ function Home(){
         <img loading="lazy" className="rounded-2xl" src="/pexels-photo-3826666.jpeg" />
         </div>
         <p className="text-white leading-7 italic font-bold text-xl"> My name is Sougata Ghar and I am a front end web developer who is proficient in HTML, CSS, Javascript,React,Vue,Next JS,Express JS and MongoDB. </p>
+        <p className="text-white leading-7 italic font-bold text-xl">My extension <a className="underline" href='https://chromewebstore.google.com/detail/image-copier-for-instagra/lejedjokmndehmcbfmpjbmimihobnfpo?hl=en&authuser=0'>Instagram Image Copier</a> is used by over 1200 people</p>
         <Link to={'/projects'}>
         <button className="btn font-bold mt-4 w-full bg-white text-black hover:bg-slate-200">Check Out My Projects</button>
         </Link>
